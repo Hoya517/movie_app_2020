fb1ad46aa4fca7f09e74e53dbf1fe3ce53112a4e
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2020-09-25 12:49:09 +0900

    0925 실습

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
 import React from 'react';
+import img01 from './images/1.jpg';
+import img02 from './images/2.jpg';
+import img03 from './images/3.jpg';
+import img04 from './images/4.jpg';
+import img05 from './images/5.jpg';
 
-function Food({fav}) {
+function Food({name, picture, alt0}) {
   return (
-    <h3>I Love {fav}</h3>
-  )
+    <div>
+      <h2>I Love {name}</h2>
+      <img src={picture} alt={alt0} />
+    </div>
+  );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: '김치',
-    // image: ' '
+    image: img01,
+    message0: '김치 사진'
   },
   {
+    id: 2,
     name: '만두',
-    // image: ' '
+    image: img02,
+    message0: '만두 사진'
   },
   {
+    id: 3,
     name: '피자',
-    // image: ' '
+    image: img03,
+    message0: '피자 사진'
+  },
+  {
+    id: 4,
+    name: '케잌',
+    image: img04,
+    message0: '케잌 사진'
+  },
+  {
+    id: 5,
+    name: '치킨',
+    image: img05,
+    message0: '치킨 사진'
   },
 ];
 
 function App() {
+  // console.log(foodILike.map(renderFood));
   return (
     <div>
-      <h1>Hello</h1>
-
+      {foodILike.map(dish =>(<Food key={dish.id} name={dish.name} picture={dish.image} alt0={dish.message0} />))}
     </div>
   );
 }
