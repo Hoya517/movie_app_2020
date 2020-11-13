9544060ee057ea8ee097ba117a474ca92ee15974
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2020-11-13 14:01:08 +0900

    1113 실습내용

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+                "안녕하세요 201740206 김주호입니다. about 페이지입니다."
+            </span>
+            <span>- 김주호</span>
+        </div>
+    );
+}
+
+export default About;
