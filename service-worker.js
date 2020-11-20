33eb635c0f04a4b91ea85d043a645206cf6c6528
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2020-11-20 12:41:43 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Hoya517/movie_app_2020.git/precache-manifest.68038a0a10cef493488ffb8ff3663c76.js"
+  "/Hoya517/movie_app_2020/precache-manifest.6eaf70c479f500272c6f4e48bb714445.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Hoya517/movie_app_2020.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Hoya517/movie_app_2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
